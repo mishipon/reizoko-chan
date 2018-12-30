0aba8247562b1682da3be9a14122fe1d1046e4c7
KATO Sayumi <sayumi@KATO-no-MacBook-Air.local>
2018-12-30 23:00:40 +0900

    Scafollding

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
